5eb974685e01c4b05557fead6426b4f51108d3ba
dylanvanyine <73609515+dylanvanyine@users.noreply.github.com>
2021-11-30 14:12:34 -0600

    Add drawing circle method

--- CirDetect.py
+++ CirDetect.py
@@ -5,6 +5,9 @@ import numpy as np
 import glob
 import math
 
+drawing = False
+ix, iy = -1, -1
+
 
 def nothing(x):
     pass
@@ -18,6 +21,25 @@ def change_img(k, i):
     elif i == len(imgs):
         i = 0
     return i, imgs[i]
+
+
+def draw_circle(event, mouse_loc_x, mouse_loc_y, flags, param):
+    global ix, iy, cx, cy, drawing, radi
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix, iy = mouse_loc_x, mouse_loc_y
+        return mouse_loc_x, mouse_loc_y
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            radi = int(math.sqrt(((ix-mouse_loc_x)**2)+((iy-mouse_loc_y)**2)))
+            cx, cy = mouse_loc_x, mouse_loc_y
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        radi = int(math.sqrt(((ix-mouse_loc_x)**2)+((iy-mouse_loc_y)**2)))
+        cx, cy = mouse_loc_x, mouse_loc_y
+        drawing = False
 
 
 imgs = glob.glob('*.jpg')
@@ -39,7 +61,7 @@ cv2.createTrackbar('V-high', 'Bars', 255, 255, nothing)
 cv2.createTrackbar('V-low', 'Bars', 120, 255, nothing)
 cv2.createTrackbar('Height', 'Bars', 40, 100, nothing)
 cv2.createTrackbar('Width', 'Bars', 40, 100, nothing)
-cv2.createTrackbar('Cir Method', 'Bars', 0, 1, nothing)
+cv2.createTrackbar('Cir Method', 'Bars', 0, 2, nothing)
 
 
 while True:
@@ -63,7 +85,6 @@ while True:
     img_HSV = cv2.cvtColor(r_img, cv2.COLOR_BGR2HSV)
     dim = (r_img.shape[1], r_img.shape[0])
     resized_dim = tuple([round(i*0.5) for i in dim])
-    # img = cv2.resize(img,resized_dim)
 
     HSV_low = np.array([Hue_low, Sat_low, Val_low])
     HSV_high = np.array([Hue_high, Sat_high, Val_high])
@@ -84,12 +105,11 @@ while True:
 
         z = np.zeros((dim[1], dim[0]), dtype=np.uint8)
         cnt = contours[0]
-        if method == 0:
+        if method == 0:  # Detect Ellipse
             el = cv2.fitEllipse(cnt)
-            # el_area = math.pi * el[1][0] * el[1][1]
             cv2.ellipse(z, el, color=255, thickness=-1)
             cv2.ellipse(result, el, (0, 255, 0), 1)
-        elif method == 1:
+        elif method == 1:  # Detect Circle
             minDist = 100
             param1 = 200  # 500
             param2 = 40  # 200 #smaller value-> more false circles
@@ -98,21 +118,20 @@ while True:
             circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1, minDist,
                                        param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
             circles = np.uint16(np.around(circles))
-            print(circles)
             for j in circles[0, :]:
                 cv2.circle(z, (j[0], j[1]), j[2], 255, -1)
                 cv2.circle(result, (j[0], j[1]), j[2], (0, 255, 0), 1)
+        elif method == 2:  # Draw Circle
+            cv2.setMouseCallback('Image', draw_circle)
+
+            cv2.circle(result, (cx, cy), radi, (0, 255, 0), thickness=1)
+            cv2.circle(z, (cx, cy), radi, 255, thickness=-1)
 
         el_mask = cv2.bitwise_and(result, result, mask=z)
         el_mask_gray = cv2.cvtColor(el_mask, cv2.COLOR_BGR2GRAY)
-        # cv2.imshow('el', el_mask)
         pixels = np.count_nonzero(el_mask_gray)
         el_pixels = np.count_nonzero(z)
         per = round((pixels/el_pixels)*100, 1)
-        # circle stuff
-        # (j, k), radius = cv2.minEnclosingCircle(cnt)
-        # center = (int(j), int(k))
-        # radius = int(radius))
 
         cv2.putText(result, str(per), (50, 50), cv2.FONT_HERSHEY_COMPLEX,
                     1, (255, 255, 150), 2, cv2.LINE_AA)
@@ -123,7 +142,6 @@ while True:
     cv2.imshow('Image', result)
     # cv2.imshow('i', z)
     # cv2.imshow('j', el_mask)
-    # print(el_mask.shape)
     k = cv2.waitKey(1)
     if k == 27:
         break
